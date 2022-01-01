e50d75e2e3152552329857a7586e89523562f842
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-01 19:40:50 +1000

    add more tests

--- src/patterns.ts
+++ src/patterns.ts
@@ -3,10 +3,13 @@ export const down = "0100";
 export const left = "1000";
 export const right = "0001";
 
-export type Note = typeof up | typeof down | typeof right | typeof down;
+export type Note = typeof up | typeof down | typeof right | typeof left;
 
 export type PatternBag = Record<string, readonly Note[]>;
 
 export const patterns: PatternBag = {
-  "urd-candle": [up, right, down] as const,
+  "urd-candle": [up, right, down],
+  "ll-double-tap": [left, left],
+  "lll-triple-tap": [left, left, left],
+  "llll-quad-tap": [left, left, left, left],
 } as const;
