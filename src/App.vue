4c8fbe04360077ce63f670dcdb84515ceb1541fb
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-09 12:47:06 +1000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { HTMLAttributes, ref } from "vue";
 import {
   addPatternDataToMeasures,
   analyzePatterns,
@@ -7,12 +6,9 @@ import {
   parse,
 } from "./analysis";
 import { patterns } from "./patterns";
-import type { Direction, Measure, NoteLineWithPatternData } from "./types";
-import { quantization } from "./noteData";
-import { chart, uberRave } from "./chart";
+import { chart } from "./chart";
 import ControlPanel from "./ControlPanel.vue";
 import { measureHeight } from "./uiConstants";
-import Arrow from "./Arrow.vue";
 import MeasureC from "./Measure.vue";
 
 const data = parse(chart);
@@ -22,127 +18,24 @@ const analysis = analyzePatterns(
   data.measures,
   patterns
 );
+
 const measures = addPatternDataToMeasures(data.measures, analysis);
-
-const quantizations = quantization.map((x) => ({
-  id: `q-${x}`,
-  quantization: x,
-  name: x === 32 || 192 ? `${x}nd` : `${x}th`,
-}));
-
-const selectedPattern = ref<string>("urd-candle");
-const selectedQuantization = ref<typeof quantizations[number]>(
-  quantizations[0]
-);
-
-const positions = new Map<Direction, number>([
-  ["left", 0],
-  ["down", 1],
-  ["up", 2],
-  ["right", 3],
-]);
-
-const measureWidth = 100;
-
-// const noteStyle = (note: NoteLineWithPatternData, direction: Direction) => {
-//   return {
-//     left: `${(measureWidth / 4) * positions.get(direction)!}px`,
-//     width: `${measureWidth / 4}px`,
-//     height: `${measureHeight / 16}`,
-//   };
-// };
-
-// const measureStyle = (
-//   lineNumber: number,
-//   measure: Measure<NoteLineWithPatternData>
-// ): HTMLAttributes["style"] => {
-//   const line = measure.notes[lineNumber - 1];
-//   const desiredPatternQuantization = line.patterns.get(selectedPattern.value)!;
-//   const highlight =
-//     desiredPatternQuantization === selectedQuantization.value.quantization;
-
-//   return {
-//     top: `${(measureHeight / measure.quantization) * (lineNumber - 1)}px`,
-//     background: highlight ? "rgba(172, 215, 230, 0.50)" : "none",
-//     height: `${measureHeight / measure.quantization}px`,
-//   };
-// };
-
-function setQuantization(q: typeof quantizations[number]) {
-  selectedQuantization.value = q;
-}
 </script>
 
 <template>
   <div id="main-container">
+    <div id="stats-container" class="border border-2">
+      Stats
+    </div>
+
     <div id="chart-container" class="border border-2">
+
       <div id="measure-container">
         <MeasureC
           v-for="measure of measures"
           :measure="measure"
           :key="measure.number"
         />
-        <!-- :style="{ height: `${measureHeight}px`, width: `${measureWidth}px` }" -->
-        <!--
-        <div
-          class="measure"
-          v-for="measure of measures"
-          :key="measure.number"
-          :style="{ height: `${measureHeight}px`, width: `${measureWidth}px` }"
-        >
-
-          <div
-            v-for="line of measure.quantization"
-            class="line"
-            :key="line"
-            :style="measureStyle(line, measure)"
-          >
-            <div
-              v-if="measure.notes[line - 1].left"
-              class="note"
-              :style="noteStyle(measure.notes[line - 1], 'left')"
-            >
-              <Arrow
-                direction="left"
-                :quantization="measure.notes[line - 1].noteQuantization"
-              />
-            </div>
-
-            <div
-              v-if="measure.notes[line - 1].down"
-              class="note"
-              :style="noteStyle(measure.notes[line - 1], 'down')"
-            >
-              <Arrow
-                direction="down"
-                :quantization="measure.notes[line - 1].noteQuantization"
-              />
-            </div>
-
-            <div
-              v-if="measure.notes[line - 1].up"
-              class="note"
-              :style="noteStyle(measure.notes[line - 1], 'up')"
-            >
-              <Arrow
-                direction="up"
-                :quantization="measure.notes[line - 1].noteQuantization"
-              />
-            </div>
-
-            <div
-              v-if="measure.notes[line - 1].right"
-              class="note"
-              :style="noteStyle(measure.notes[line - 1], 'right')"
-            >
-              <Arrow
-                direction="right"
-                :quantization="measure.notes[line - 1].noteQuantization"
-              />
-            </div>
-          </div>
-        </div>
-        -->
       </div>
     </div>
 
@@ -152,13 +45,6 @@ function setQuantization(q: typeof quantizations[number]) {
 
     <div id="bottom-half" class="border border-2">bottom</div>
   </div>
-
-  <!-- <div class="flex"> -->
-
-  <!-- 
-    -->
-
-  <!-- </div> -->
 </template>
 
 <style scoped>
@@ -172,7 +58,7 @@ function setQuantization(q: typeof quantizations[number]) {
 
 #main-container {
   display: grid;
-  grid-template-columns: 3fr 1fr;
+  grid-template-columns: 1fr 3fr 1fr;
   grid-template-rows: 5fr 1fr;
   height: 100vh;
 }
@@ -191,7 +77,6 @@ function setQuantization(q: typeof quantizations[number]) {
   width: 200px;
   display: grid;
   grid-template-columns: 1fr;
-  /* gap: 2px; */
   grid-auto-rows: v-bind("measureHeight.px");
 }
 </style>
